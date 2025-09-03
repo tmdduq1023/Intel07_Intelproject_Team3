730739c40e6a248fb6605592a9b902e524e0138b
bbangx2 <hkt7136@gmail.com>
2025-09-03 13:40:24 +0900

    AI ver 0.3

--- preprocess.py
+++ preprocess.py
@@ -6,18 +6,16 @@ import shutil
 
 def preprocess_images(base_path, new_size=(224, 224)):
     """
-    COCO JSON 파일을 기반으로 이미지들을 리사이즈하여 새 폴더에 저장합니다.
-    JSON 파일도 함께 복사합니다.
+    COCO JSON 파일을 기반으로 이미지들을 리사이즈하여 새 폴더에 저장함
+    JSON 파일도 함께 복사됨
     """
-    # --- 경로 설정 ---
-    # 원본 JSON 파일
+
     json_path = os.path.join(base_path, 'coco_skin_dataset.json') 
     
     # 전처리된 데이터를 저장할 새 폴더
     output_base_dir = os.path.join(base_path, 'dataset_preprocessed')
     
     # --- 폴더 생성 ---
-    # 출력 폴더가 이미 있다면 삭제하고 새로 시작 (선택사항)
     if os.path.exists(output_base_dir):
         print(f"'{output_base_dir}' exists. Removing it to start fresh.")
         shutil.rmtree(output_base_dir)
@@ -35,7 +33,7 @@ def preprocess_images(base_path, new_size=(224, 224)):
         original_img_path = os.path.join(base_path, img_info['file_name'])
         new_img_path = os.path.join(output_base_dir, img_info['file_name'])
 
-        # 이미지 파일이 저장될 하위 폴더 생성 (예: 'images/')
+        # 이미지 파일이 저장될 하위 폴더 생성
         os.makedirs(os.path.dirname(new_img_path), exist_ok=True)
 
         try:
@@ -55,6 +53,6 @@ def preprocess_images(base_path, new_size=(224, 224)):
 
 
 if __name__ == '__main__':
-    # 이 스크립트가 있는 폴더(프로젝트 루트)를 기준으로 경로를 설정합니다.
+    # 이 스크립트가 있는 폴더(프로젝트 루트)를 기준으로 경로를 설정
     project_base_path = os.path.dirname(os.path.abspath(__file__))
     preprocess_images(base_path=project_base_path)
